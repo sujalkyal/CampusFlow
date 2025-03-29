0b4184517c1d1eb212281743cc47527a7c0e1981
sujalkyal <sujalkyal2704@gmail.com>
2025-03-29 17:27:08 +0530

    student auth completed

--- apps/student/app/api/batch/getAllBatches/route.js
+++ apps/student/app/api/batch/getAllBatches/route.js
@@ -2,14 +2,8 @@
 
 import { NextResponse } from "next/server";
 import prisma from "@repo/db/client";
-import { getServerSession } from "next-auth/next";
-import { authOptions } from "@/lib/auth";
 
-export async function GET(req) {
-  const session = await getServerSession(authOptions);
-  if (!session) {
-    return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
-  }
+export async function POST(req) {
   try{
     const { dept_id } = await req.json();
     if (!dept_id) {
